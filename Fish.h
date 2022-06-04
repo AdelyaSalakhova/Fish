1b4469db785cc9aadc25d0b0acb69de86bbac6c0
AdelyaSalakhova <99472736+AdelyaSalakhova@users.noreply.github.com>
2022-06-05 02:14:46 +0300

    Update Fish.h

--- Fish.h
+++ Fish.h
@@ -13,16 +13,17 @@ protected:
 	Image image;
 	Texture texture;
 	Vector2i RightOrigin, LeftOrigin;
-	int dir, FishNumber;
+	int dir, FishNumber, c, feed;
 	Clock clock;
 public:
-	Fish(string F, float x, float y, float Rs1, float Rs2, float w, float h); //  онструктор, определ€ютс€ размеры спрайтов, координаты, направление. скорость и т.д.
-	void BreathUp(); // ¬сплытие вверх за кислородом.
-	void BreathDown(); // ¬озвращение на траекторию после всплыти€ за кислородом.
+	Fish(string F, float x, float y, float Rs1, float Rs2, float w, float h); 
+	void BreathUp(); 
+    void BreathDown(); 
 	void EatingUp();
 	void EatingDown();
 	void Checking();
-	void Update(); // ќтвечает за посто€нное движение рыб по траектории и смену направлени€.
+	virtual void Update() = 0; 
+	virtual bool Breath() = 0;
 	int GetFishNumber();
 	float GetX();
 	void SetX(float x);
@@ -36,24 +37,22 @@ public:
 };
 
 class User {
-protected :
-	Texture menu;
-	Sprite menus;
-	Texture food;
-	Sprite foods;
-	Texture info;
-	Sprite infos;
-	bool desision;
+protected:
+	Texture menu, food, info;
+	Sprite menus, foods, infos;
 	int c, feed;
-	bool GF, BF, DF, CF;
+	bool GF, BF, DF, CF, desision;
 	Event UserEvent;
 	vector <Fish*> fish;
-public :
+public:
 	void ToFeed(RenderWindow& window, Sprite& herosprite);
 	void ToCreate(RenderWindow& window);
 	void ToDelete(RenderWindow& window);
 	void GetInfo(RenderWindow& window);
 	void Show(RenderWindow& window, Sprite& herosprite);
+	void Draw(RenderWindow& window, Sprite& herosprite);
+	void Up(int i, RenderWindow& window, Sprite& herosprite);
+	void Down(int i, RenderWindow& window, Sprite& herosprite);
 	Event& GetUserEvent();
 	vector <Fish*>& GetFish();
 	User();
@@ -61,23 +60,33 @@ public :
 };
 
 class GoldenFish : public Fish {
-private :
-
 public:
 	GoldenFish();
+	void Update() override;
+	bool Breath() override;
 };
 
 class CatFish : public Fish {
+private :
+	bool rep;
 public:
 	CatFish();
+	void Update() override;
+	bool Breath() override;
 };
 
 class BettaFish : public Fish {
 public:
 	BettaFish();
+	void Update() override;
+	bool Breath() override;
 };
 
 class DanioFish : public Fish {
+private :
+	float counter;
 public:
 	DanioFish();
+	void Update() override;
+	bool Breath() override;
 };
